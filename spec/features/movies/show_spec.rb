96372f58a0282dda25a79654b4056c2c9f64f00b
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-04 12:51:17 -0600

    test: page has links to create a viewing party and to return to discover page

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe 'movie show page' do
     end
     it 'shows a movies title and other details', :vcr do
       visit "/users/#{@user.id}/movies/#{@movie.id}"
-save_and_open_page
+
       expect(page).to have_content("The Shawshank Redemption")
       expect(page).to have_content("2h 22m")
       expect(page).to have_content("Imprisoned in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.")
@@ -39,11 +39,19 @@ save_and_open_page
     end
 
     it 'contains a link to return to the discover page' do
-      expect(page).to have_link("Return to Discover Movies", href: "users/#{@user.id}/discover")
+      VCR.use_cassette("/movie_show_page/displays_a_movies_information/shows_a_movies_title_and_other_details") do
+        visit "/users/#{@user.id}/movies/#{@movie.id}"
+
+        expect(page).to have_link("Return to Discover Movies")
+      end
     end
 
     it 'contains a link to create a viewing party' do
-      expect(page).to have_link("Create Viewing Party")
+      VCR.use_cassette("/movie_show_page/displays_a_movies_information/shows_a_movies_title_and_other_details") do
+        visit "/users/#{@user.id}/movies/#{@movie.id}"
+
+        expect(page).to have_link("Create Viewing Party")
+      end
     end
   end
 end
