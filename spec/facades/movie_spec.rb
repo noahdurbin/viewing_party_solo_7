f8e6da4021dd3f207f8c8a7e16f5088dcb38be9c
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-03 21:36:20 -0600

    test: test movie facade

---
+++ spec/facades/movie_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe MovieFacade do
+  it 'can retrieve the top rated movies' do
+    VCR.use_cassette('/top_movie_discovery/happy_path/allows_a_user_to_discover_the_top_rated_movies') do
+      facade = MovieFacade.new
+      response = facade.top_rated
+
+      expect(facade).to be_a MovieFacade
+      expect(response).to be_a Array
+      expect(response).to be_all Movie
+    end
+  end
+
+  it 'can search for a movie with a keyword' do
+    VCR.use_cassette('/movieservice/should_return_a_response_body_parsed_from_JSON_when_given_a_search_parameter') do
+      facade = MovieFacade.new
+      response = facade.search('Interstellar')
+
+      expect(facade).to be_a MovieFacade
+      expect(response).to be_a Array
+      expect(response).to be_all Movie
+    end
+  end
+end
