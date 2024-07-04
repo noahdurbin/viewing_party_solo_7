53e8aebee15bdbbb4d60893f64ee0bc8ce664db4
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-04 10:57:36 -0600

    feat: added initialize methods and structs to the movie poro

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,4 +1,7 @@
 class Movie
+  CastMember = Struct.new(:id, :name, :character)
+  Review = Struct.new(:author, :content)
+
   attr_reader :title,
               :score,
               :id
@@ -7,5 +10,36 @@ class Movie
     @id = attributes[:id]
     @title = attributes[:title]
     @score = attributes[:vote_average]
+    @genres = set_genres(attributes[:genres])
+    @runtime = set_runtime(attributes[:runtime])
+    @summary = attributes[:overview]
+    @tagline = attributes[:tagline]
+    @top_cast = set_top_cast(attributes[:cast])
+    @review = set_reviews(attributes[:reviews])
+  end
+
+  def set_genres(genre_array)
+    genre_array.map do |genre|
+      genre[:name]
+    end
+  end
+
+  def set_runtime(runtime)
+    hours = runtime / 60
+    minutes = runtime % 60
+    "#{hours}h #{minutes}m"
+  end
+
+  def set_top_cast(total_cast)
+    total_cast.first(10).map do |cast_member|
+      CastMember.new(cast_member[:id], cast_member[:name], cast_member[:character])
+    end
+  end
+
+  def set_reviews(review_list)
+    results = review_list[:results]
+    results.map do |review|
+      Review.new(review[:author], review[:content])
+    end
   end
 end
