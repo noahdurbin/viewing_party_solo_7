cefeb239fdaa958d233647deaae31a59c8eb3071
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-04 21:44:37 -0600

    test: initialization and cleaned up comments from code

--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -1,18 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Movie do
-  it 'can create a new movie object from hash data' do
-    test_data = {
-      title: 'Interstellar',
-      id: 12,
-      vote_average: 10.0
-    }
+  describe 'Initialize' do
+    it 'can create a new movie object from search hash data' do
+      test_data = {
+        title: 'Interstellar',
+        id: 12,
+        vote_average: 10.0
+      }
 
-    movie = Movie.new(test_data)
+      movie = Movie.new(test_data)
 
-    expect(movie).to be_a(Movie)
-    expect(movie.title).to eq('Interstellar')
-    expect(movie.score).to eq(10.0)
+      expect(movie).to be_a(Movie)
+      expect(movie.title).to eq('Interstellar')
+      expect(movie.score).to eq(10.0)
+    end
+
+    it 'can create a movie object from the full movie details' do
+      test_data = {
+        id: 123,
+        title: "Test Movie",
+        vote_average: 8.5,
+        genres: [{ name: "Action" }, { name: "Drama" }],
+        runtime: 150,
+        overview: "A great movie about testing",
+        tagline: "Test all the things",
+        credits: {
+          cast: [
+            { id: 1, name: "Actor 1", character: "Character 1" },
+            { id: 2, name: "Actor 2", character: "Character 2" }
+          ]
+        },
+        reviews: {
+          results: [
+            { author: "Reviewer 1", content: "Great movie!" },
+            { author: "Reviewer 2", content: "Loved it!" }
+          ]
+        }
+      }
+
+      movie = Movie.new(test_data)
+
+      expect(movie.id).to eq(123)
+      expect(movie.title).to eq("Test Movie")
+      expect(movie.score).to eq(8.5)
+      expect(movie.genres).to eq(["Action", "Drama"])
+      expect(movie.runtime).to eq("2h 30m")
+      expect(movie.runtimemin).to eq(150)
+      expect(movie.summary).to eq("A great movie about testing")
+      expect(movie.tagline).to eq("Test all the things")
+    end
   end
 
   describe 'methods' do
@@ -87,52 +124,3 @@ RSpec.describe Movie do
     end
   end
 end
-
-
-# require 'rails_helper'
-
-# RSpec.describe 'movie show page' do
-#   describe 'displays a movies information' do
-#     before :each do
-#       @user = User.create(name: 'Noah', email: 'ndurbin@gmail.com')
-#       @movie = Movie.new(
-#         id: 123,
-#         title: "Example Movie",
-#         vote_average: 8.5,
-#         genres: [{id: 28, name: "Action"}, {id: 12, name: "Adventure"}],
-#         runtime: 142,
-#         overview: "This is a summary of the movie.",
-#         tagline: "An exciting tagline!",
-#         cast: [
-#           {id: 1, name: "Actor 1", character: "Character 1"},
-#           {id: 2, name: "Actor 2", character: "Character 2"},
-#         ],
-#         reviews: {
-#           page: 1,
-#           results: [
-#             {author: "Reviewer 1", content: "Great movie!"},
-#             {author: "Reviewer 2", content: "I loved it."},
-#           ]
-#         }
-#       )
-#     end
-#     it 'shows a movies title and other details' do
-#       visit "/users/#{@user.id}/movies/#{@movie.id}"
-
-#       expect(page).to have_content("Example Movie")
-#       expect(page).to have_content("2h 22m")
-#       expect(page).to have_content("This is a summary of the movie.")
-#       expect(page).to have_content("An exciting tagline!")
-#       expect(page).to have_content("Great movie")
-#       expect(page).to have_content("Actor 1")
-#     end
-
-#     it 'contains a link to return to the discover page' do
-#       expect(page).to have_link("Return to Discover Movies", href: "users/#{@user.id}/discover")
-#     end
-
-#     it 'contains a link to create a viewing party' do
-#       expect(page).to have_link("Create Viewing Party")
-#     end
-#   end
-# end
