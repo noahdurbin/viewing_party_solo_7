57d42effad7a95a8e996f4ee3161931207878281
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-04 21:55:53 -0600

    test: movie service tests

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe MovieService do
     expect(response).to all(be_a Movie)
     expect(response.first.title).to eq('Interstellar')
   end
+
+  it 'should return a list of 20 movies', :vcr do
+    response = MovieService.new.top_rated
+
+    expect(response).to be_a Array
+    expect(response).to all(be_a Movie)
+    expect(response.length).to eq 20
+    expect(response.first.title).to eq("The Shawshank Redemption")
+  end
+
+  it 'should be able to retrieve a moviews details', :vcr do
+    response = MovieService.new.movie_details(278)
+
+    expect(response).to be_a Movie
+    expect(response.title).to eq("The Shawshank Redemption")
+    expect(response.score).to eq(8.705)
+    expect(response.top_cast.length).to eq(10)
+  end
 end
